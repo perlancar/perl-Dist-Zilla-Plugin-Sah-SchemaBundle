d889f293210b6e80b643f0c2c536a1fcb4b20467
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-07-22 13:26:50 +0700

    Produce Sah::SchemaR::* modules (pre-resolved edition of schemas)

--- lib/Dist/Zilla/Plugin/Sah/Schemas.pm
+++ lib/Dist/Zilla/Plugin/Sah/Schemas.pm
@@ -11,6 +11,7 @@ use Moose;
 use namespace::autoclean;
 
 with (
+    'Dist::Zilla::Role::FileGatherer',
     'Dist::Zilla::Role::FileMunger',
     'Dist::Zilla::Role::FileFinderUser' => {
         default_finders => [':InstallModules'],
@@ -18,111 +19,197 @@ with (
     'Dist::Zilla::Role::PrereqSource',
 );
 
+sub _load_schema_modules {
+    my $self = shift;
+
+    return $self->{_our_schema_modules} if $self->{_loaded_schema_modules}++;
+
+    my %res;
+    for my $file (@{ $self->found_files }) {
+        unless ($file->isa("Dist::Zilla::File::OnDisk")) {
+            $self->log_debug(["skipping %s: not an ondisk file, currently only ondisk files are processed", $file->name]);
+            next;
+        }
+        next unless $file->name =~ m!^lib/(Sah/Schema/.+\.pm)$!;
+        my $pkg_pm = $1;
+        (my $pkg = $pkg_pm) =~ s/\.pm$//; $pkg =~ s!/!::!g;
+        {
+            local @INC = ("lib", @INC);
+            require $pkg_pm;
+            $res{$pkg} = $file;
+        }
+    }
+
+    $self->{_our_schema_modules} = \%res;
+}
+
+sub _load_schemas_modules {
+    my $self = shift;
+
+    return $self->{_our_schemas_modules} if $self->{_loaded_schemas_modules}++;
+
+    my %res;
+    for my $file (@{ $self->found_files }) {
+        unless ($file->isa("Dist::Zilla::File::OnDisk")) {
+            $self->log_debug(["skipping %s: not an ondisk file, currently only ondisk files are processed", $file->name]);
+            next;
+        }
+        next unless $file->name =~ m!^lib/(Sah/Schemas/.+\.pm)$!;
+        my $pkg_pm = $1;
+        (my $pkg = $pkg_pm) =~ s/\.pm$//; $pkg =~ s!/!::!g;
+        {
+            local @INC = ("lib", @INC);
+            require $pkg_pm;
+            $res{$pkg} = $file;
+        }
+    }
+
+    $self->{_our_schemas_modules} = \%res;
+}
+
 sub munge_files {
     no strict 'refs';
 
     my $self = shift;
 
-    $self->{_our_schema_modules}  //= {};
     $self->{_used_schema_modules} //= {};
 
-    for my $file (@{ $self->found_files }) {
-        unless ($file->isa("Dist::Zilla::File::OnDisk")) {
-            $self->log_debug(["skipping %s: not an ondisk file, currently generated file is assumed to be OK", $file->name]);
-            return;
-        }
-        my $file_name = $file->name;
+    $self->_load_schema_modules;
+    $self->_load_schemas_modules;
+
+    for my $pkg (sort keys %{ $self->{_our_schemas_modules} }) {
+        # ...
+    }
+
+    for my $pkg (sort keys %{ $self->{_our_schema_modules} }) {
+        my $file = $self->{_our_schema_modules}{$pkg};
+
         my $file_content = $file->content;
-        if ($file_name =~ m!^lib/((.+)\.pm)$!) {
-            my $package_pm = $1;
-            my $package = $2; $package =~ s!/!::!g;
 
-            if ($package =~ /^Sah::Schema::/) {
-                {
-                    local @INC = ("lib", @INC);
-                    require $package_pm;
-                    $self->{_our_schema_modules}{$package}++;
-                }
-                my $sch = ${"$package\::schema"};
-                my $nsch = Data::Sah::Normalize::normalize_schema($sch);
+        my $sch = ${"$pkg\::schema"} or do {
+            $self->log_fatal(["No schema defined in \$schema in %s", $file->name]);
+        };
+        my $nsch = Data::Sah::Normalize::normalize_schema($sch);
 
-                # check that schema is already normalized
-                {
-                    require Data::Dump;
-                    require Data::Sah::Normalize;
-                    require Text::Diff;
-                    my $sch_dmp  = Data::Dump::dump($sch);
-                    my $nsch_dmp = Data::Dump::dump($nsch);
-                    last if $sch_dmp eq $nsch_dmp;
-                    my $diff = Text::Diff::diff(\$sch_dmp, \$nsch_dmp);
-                    $self->log_fatal("Schema in $file_name is not normalized, below is the dump diff (- is current, + is normalized): " . $diff);
-                }
-
-                # collect other Sah::Schema::* modules that are used, this will
-                # be added as prereq
-              COLLECT_BASE_SCHEMAS:
-                {
-                    require Data::Sah::Normalize;
-                    require Data::Sah::Resolve;
-                    require Data::Sah::Util::Subschema;
-
-                    $self->log_debug(["Finding schema modules required by %s", $package]);
-
-                    my $subschemas;
-                    eval {
-                        $subschemas = Data::Sah::Util::Subschema::extract_subschemas(
-                            {schema_is_normalized => 1},
-                            $nsch,
-                        );
-                    };
-                    if ($@) {
-                        $self->log(["Can't extract subschemas from schema in %s (%s), skipped", $package, $@]);
-                        last COLLECT_BASE_SCHEMAS;
-                    }
-
-                    for my $subsch ($nsch, @$subschemas) {
-                        my $nsubsch = Data::Sah::Normalize::normalize_schema($subsch);
-                        my $res = Data::Sah::Resolve::resolve_schema(
-                            {
-                                schema_is_normalized => 1,
-                                return_intermediates => 1,
-                            },
-                            $nsubsch);
-                        my $intermediates = $res->[2];
-                        for my $i (0..$#{$intermediates}-1) {
-                            my $mod = "Sah::Schema::$intermediates->[$i]";
-                            $self->{_used_schema_modules}{$mod}++;
-                        }
-                    }
-                }
-
-                # set ABSTRACT from schema's summary
-                {
-                    unless ($file_content =~ m{^#[ \t]*ABSTRACT:[ \t]*([^\n]*)[ \t]*$}m) {
-                        $self->log_debug(["Skipping setting ABSTRACT %s: no # ABSTRACT", $file_name]);
-                        last;
-                    }
-                    my $abstract = $1;
-                    if ($abstract =~ /\S/) {
-                        $self->log_debug(["Skipping setting ABSTRACT %s: already filled (%s)", $file_name, $abstract]);
-                        last;
-                    }
-
-                    $file_content =~ s{^#\s*ABSTRACT:.*}{# ABSTRACT: $sch->[1]{summary}}m
-                        or die "Can't set abstract for " . $file->name;
-                    $self->log(["setting abstract for %s (%s)", $file->name, $sch->[1]{summary}]);
-                    $file->content($file_content);
-                }
-            } # Sah::Schema::*
+        # check that schema is already normalized
+        {
+            require Data::Dump;
+            require Data::Sah::Normalize;
+            require Text::Diff;
+            my $sch_dmp  = Data::Dump::dump($sch);
+            my $nsch_dmp = Data::Dump::dump($nsch);
+            last if $sch_dmp eq $nsch_dmp;
+            my $diff = Text::Diff::diff(\$sch_dmp, \$nsch_dmp);
+            $self->log_fatal(["Schema in %s is not normalized, below is the dump diff (- is current, + is normalized): %s", $file->name, $diff]);
         }
-    } # for $file
+
+        # collect other Sah::Schema::* modules that are used, this will
+        # be added as prereq
+      COLLECT_BASE_SCHEMAS:
+        {
+            require Data::Sah::Normalize;
+            require Data::Sah::Resolve;
+            require Data::Sah::Util::Subschema;
+
+            $self->log_debug(["Finding schema modules required by %s", $pkg]);
+
+            my $subschemas;
+            eval {
+                $subschemas = Data::Sah::Util::Subschema::extract_subschemas(
+                    {schema_is_normalized => 1},
+                    $nsch,
+                );
+            };
+            if ($@) {
+                $self->log(["Can't extract subschemas from schema in %s (%s), skipped", $pkg, $@]);
+                last COLLECT_BASE_SCHEMAS;
+            }
+
+            for my $subsch ($nsch, @$subschemas) {
+                my $nsubsch = Data::Sah::Normalize::normalize_schema($subsch);
+                my $res = Data::Sah::Resolve::resolve_schema(
+                    {
+                        schema_is_normalized => 1,
+                        return_intermediates => 1,
+                    },
+                    $nsubsch);
+                my $intermediates = $res->[2];
+                for my $i (0..$#{$intermediates}-1) {
+                    my $mod = "Sah::Schema::$intermediates->[$i]";
+                    $self->{_used_schema_modules}{$mod}++;
+                }
+            }
+        }
+
+        # set ABSTRACT from schema's summary
+        {
+            unless ($file_content =~ m{^#[ \t]*ABSTRACT:[ \t]*([^\n]*)[ \t]*$}m) {
+                $self->log_debug(["Skipping setting ABSTRACT %s: no # ABSTRACT", $file->name]);
+                last;
+            }
+            my $abstract = $1;
+            if ($abstract =~ /\S/) {
+                $self->log_debug(["Skipping setting ABSTRACT %s: already filled (%s)", $file->name, $abstract]);
+                last;
+            }
+
+            $file_content =~ s{^#\s*ABSTRACT:.*}{# ABSTRACT: $sch->[1]{summary}}m
+                or die "Can't set abstract for " . $file->name;
+            $self->log(["setting abstract for %s (%s)", $file->name, $sch->[1]{summary}]);
+            $file->content($file_content);
+        }
+
+        # create lib/Sah/SchemaR/*.pm
+        {
+            require Data::Dump;
+            require Data::Sah::Resolve;
+            require Dist::Zilla::File::InMemory;
+
+            my $rschema = Data::Sah::Resolve::resolve_schema(
+                {return_intermediates => 1},
+                $sch,
+            );
+            my $rname = $file->name; $rname =~ s!^lib/Sah/Schema/!lib/Sah/SchemaR/!;
+            my $rpkg  = $pkg; $rpkg =~ s/^Sah::Schema::/Sah::SchemaR::/;
+            my $rfile = Dist::Zilla::File::InMemory->new(
+                name => $rname,
+                content => join(
+                    "",
+                    "package $rpkg;\n",
+                    "\n",
+
+                    "# DATE\n",
+                    "# VERSION\n",
+                    "\n",
+
+                    "our \$rschema = ", Data::Dump::dump($rschema), ";\n",
+                    "\n",
+
+                    "1;\n",
+                    "# ABSTRACT: $sch->[1]{summary}\n",
+                    "\n",
+
+                    "=head1 DESCRIPTION\n\n",
+                    "This module is automatically generated by ".__PACKAGE__." during distribution build.\n\n",
+                ),
+            );
+            $self->log(["Creating file %s", $rname]);
+            $self->add_file($rfile);
+        }
+
+    } # Sah::Schema::*
+}
+
+sub gather_files {
+    # we add files in the munge_files() phase because at this point,
+    # found_files() doesn't yet work
 }
 
 sub register_prereqs {
     my $self = shift;
 
     #use DD; dd $self->{_used_schema_modules}; dd $self->{_our_schema_modules};
-    for my $mod (sort keys %{$self->{_used_schema_modules}}) {
+    for my $mod (sort keys %{$self->{_used_schema_modules} // {}}) {
         next if $self->{_our_schema_modules}{$mod};
         $self->log(["Adding prereq to %s", $mod]);
         $self->zilla->register_prereqs({phase=>'runtime'}, $mod);
@@ -164,6 +251,12 @@ Otherwise, the build is aborted.
 =item * Set module abstract from the schema's summary
 
 =item * Add a prereq to other Sah::Schema::* module if schema depends on those other schemas
+
+=item * Produce pre-resolved editions of schemas into C<lib/Sah/SchemaR/*>
+
+These are useful if a client wants to lookup the base type of a schema wihout
+having to do any extra resolving. Currently L<Perinci::Sub::Complete> uses this
+to reduce startup overhead when doing tab completion.
 
 =back
 
